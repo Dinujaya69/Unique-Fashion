8b76824ff00b50b00f0e89401c849f2bd986b172
Dinujaya69 <kavinduweerasinha4@gmai.com>
2024-07-19 14:18:05 +0530

    full website

---
+++ script.js
@@ -0,0 +1,36 @@
+
+$('.slides').slick({
+    dots: false,
+    infinite: false,
+    autoplay:true,
+    arrows:false,   
+    speed: 200,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+     
+    ]
+  });
